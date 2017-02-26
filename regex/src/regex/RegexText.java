a05cb0e557f57c6dbb772a2352186c3ef20fd429
xsw9898 <xushaowei_98@163.com>
2017-02-26 15:50:16 +0800

    test

---
+++ regex/src/regex/RegexText.java
@@ -0,0 +1,63 @@
+package regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class RegexText {
+
+	public static void main(String[] args) throws PatternSyntaxException
+	{
+		Scanner in =new Scanner(System.in);
+		System.out.println("Enter pattern:");
+		String patternString = in.nextLine();
+		
+		Pattern pattern = Pattern.compile(patternString);
+		
+		while(true){
+			System.out.println("Enter string to match:");
+			String input = in.nextLine();
+			if(input == null || input.equals(""))
+			{
+				return;
+			}
+			Matcher matcher = pattern.matcher(input);
+			if(matcher.matches())
+			{
+				System.out.println("Match");
+				int g = matcher.groupCount();
+				if(g > 0)
+				{
+					for(int i = 0; i < input.length(); i++){
+						for(int j = 1; j <= g; j++)
+						{
+							if(i == matcher.start(j) && i == matcher.end(j))
+							{
+								System.out.println("()");
+							}
+						}
+						for(int j = 1; j <= g; j++){
+							if(i == matcher.start(j) && i != matcher.end(j))
+							{
+								System.out.println('(');
+							}
+						}
+						System.out.println(input.charAt(i));
+						for(int j = 1; j <= g; j++){
+							if(i + 1 != matcher.start(j) && i + 1 == matcher.end(j))
+							{
+								System.out.println(')');
+							}
+						}
+					}
+					System.out.println();
+				}
+			}else{
+				System.out.println("No match");
+			}
+		}
+
+	}
+
+}
